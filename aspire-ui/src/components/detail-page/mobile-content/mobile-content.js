3f80277d3ddc921759c75a9387d333c03c214f2c
Akash Singh <akashs@ojolabs.com>
2022-10-03 17:25:58 +0530

    work in progress

--- aspire-ui/src/components/detail-page/mobile-content/mobile-content.js
+++ aspire-ui/src/components/detail-page/mobile-content/mobile-content.js
@@ -1,8 +1,12 @@
 import cardContentHead from "../card-content/card-content-head/card-content-head.vue";
+import manageCard from "../card-content/manage-card/manage-card.vue";
+import cardContent from "../card-content/card-content.vue";
 
 export default {
     name: 'mobileContent',
     components: {
-        cardContentHead
+        cardContentHead,
+        manageCard,
+        cardContent
     }
 }
--- aspire-ui/src/components/detail-page/mobile-content/mobile-content.vue
+++ aspire-ui/src/components/detail-page/mobile-content/mobile-content.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mobile-content">
-        <cardContentHead />
+        <cardContent />
     </div>
 </template>
 
